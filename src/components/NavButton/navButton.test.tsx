603f11b2c358af1941bc4090b03c7dab03e95079
Malek Smida <malek.ssmida@gmail.com>
2025-03-13 17:11:28 +0100

    refactor: navButton test

--- src/components/NavButton/navButton.test.tsx
+++ src/components/NavButton/navButton.test.tsx
@@ -1,11 +1,18 @@
-import { expect, test, describe } from 'vitest';
-import { render, screen } from '@testing-library/react';
+import { expect, test, describe, afterEach } from 'vitest';
+import { cleanup, render, screen } from '@testing-library/react';
+
+// local files
 import NavButton from './NavButton';
 
+// Constants
 const dummyTitle = 'dummy title',
-  dummyAnchorLink = 'https://www.test.com/';
+  dummyAnchorLink = 'dummy anchor link';
 
 describe('NavButton Component', () => {
+  afterEach(() => {
+    cleanup();
+  });
+
   test('should renders the navigation button component without errors', () => {
     render(<NavButton title={dummyTitle} anchorLink={dummyAnchorLink} />);
 
@@ -13,6 +20,12 @@ describe('NavButton Component', () => {
     expect(
       screen.getByRole('link', { name: `Navigate to the section ${dummyTitle}` }),
     ).toBeDefined();
+
+    /**
+     * For information:
+     * Could not test href value as it turns out that Vitest does not have toHaveAttribute() funtion.
+     * An alternative is to use "data-testid" : https://stackoverflow.com/a/78227205/13987596
+     */
 
     expect(screen.getByText(dummyTitle)).toBeDefined();
   });
