a40ac718a66e4f1cf05dbad547501025b8a28e30
Malek Smida <malek.ssmida@gmail.com>
2025-03-14 20:48:43 +0100

    reafctor: nav button structure

--- src/components/NavButton/NavButton.tsx
+++ src/components/NavButton/NavButton.tsx
@@ -1,19 +1,16 @@
 import { INavButton } from './navButton.types';
 
 const NavButton: React.FC<INavButton> = ({ title, anchorLink }) => {
-  const handleClick = () => {
-    // TODO:
-  };
-
   return (
-    <a
-      href={anchorLink}
-      onClick={handleClick}
-      aria-label={`Navigate to the section ${title}`}
-      className="before:bg-primary-color hover:text-primary-color relative cursor-pointer text-sm font-medium text-gray-500 transition-colors duration-300 ease-in-out select-none before:absolute before:-bottom-1 before:h-0.5 before:w-full before:origin-left before:scale-x-0 before:transition hover:before:scale-100"
-    >
-      {title}
-    </a>
+    <li>
+      <a
+        href={anchorLink}
+        aria-label={`Navigate to the section ${title}`}
+        className="before:bg-primary hover:text-primary relative cursor-pointer text-sm font-medium text-gray-500 transition-colors duration-300 ease-in-out select-none before:absolute before:-bottom-1 before:h-0.5 before:w-full before:origin-left before:scale-x-0 before:transition hover:before:scale-100"
+      >
+        {title}
+      </a>
+    </li>
   );
 };
 
